390332cf66944be8c296dca200bdb2ad60f4f229
avni jain <javni647@gmail.com>
2019-04-23 08:55:37 +0530

    links to add - zines

--- assets/nav.js
+++ assets/nav.js
@@ -60,8 +60,6 @@
       scrollLeft:init_left}, 300);
 
 
-    // console.log(init_top)
-
     })
 
       var window_height = $(window).height();
@@ -93,14 +91,14 @@
   // console.log(init_top)
   //     	scroll_to(init_top,init_left)
 
-
+  var image_width = $("#overview").width()
+  var image_height = $("#overview").height()
 // $("#slide1").css('opacity', 0);
 $('#overview').on('click', function(e){
 
 
             if($('.zoom-out').is(':visible')){
-              var image_width = $("#overview").width()
-              var image_height = $("#overview").height()
+
             $("#overview").removeClass("zoom-out").addClass("zoom-in")
       			// $(".overview").removeClass("zoom-out").addClass("zoom-in");
       			// if($("#center").length != 0) {
@@ -108,20 +106,16 @@ $('#overview').on('click', function(e){
       			// }
       			$("#overview").css({"height":84 + 'vw', "width":window_width})
             	$(".click_layer").css({"height":84 + 'vw', "width":window_width})
-                	$(".click_layer").css('opacity',1)
-            // scroll_to({'top':0,'left':0})
+
+            scroll_to({'top':0,'left':0})
 
             // console.log(window_height);
 }else{
   // $(".overview.zoom-in").on('click',function(){
 
-  $("#overview").removeClass("zoom-in").addClass("zoom-out")
-  $("#overview").css({"height":2200, "width":2600})
-  	// $(".click_layer").css('opacity',0)
 
-  $(".click_layer").css({"height":2200 , "width":2600})
-       var offset_top = e.pageY;
-       var offset_left = e.pageX;
+       var offset_top = event.pageY;
+       var offset_left = event.pageX;
        // var window_height = $(window).height();
        // var window_width= $(window).width();
        // var image_width = $(".overview").width()
@@ -129,11 +123,25 @@ $('#overview').on('click', function(e){
        var overview_image_width = $("#overview").width()
        var overview_image_height = $("#overview").height()
 
-       var top = Math.round(((offset_top*2200)/694)-(window_height/2));
-       var left = Math.round(((offset_left*2600)/849)-(window_width/2));
+       // var top = Math.round(((offset_top*image_height)/2600)-(window_height));
+       // var left = Math.round(((offset_left*image_width)/2200)-(window_width));
+      //  var top = Math.round(((offset_top*image_height)/overview_image_height)-(window_height/2));
+ 			// var left = Math.round(((offset_left*image_width)/overview_image_width)-(window_width/2));
+    // document.scrollTo(top,left)
+$('html, body').animate({scrollTop:offset_top, scrollLeft:offset_left},50);
+$("#overview").removeClass("zoom-in").addClass("zoom-out")
+$("#overview").css({"height":2200, "width":2600})
+  // $(".click_layer").css('opacity',0)
 
-     scroll_to(top,left)
-       console.log(window_width)
+$(".click_layer").css({"height":2200 , "width":2600})
+    //   $('html,body').animate({
+    //  scrollTop: top,
+    //   scrollLeft:left}, 300);
+    //
+    //
+    // })
+       console.log(top)
+       // console.log(left)
     //    $(this).removeClass("zoom-in").addClass("zoom-out")
     //    var this_top = $(this).offset().top
     //    var this_left = $(this).offset().left
@@ -146,14 +154,16 @@ $('#overview').on('click', function(e){
 }
 
 
-            if($('.zoom-in').is(':visible')){
-              // $(".click_layer").css({"height":2200 + "width":2600})
-            }
 
 })
-$('.shape_1').on('click',function (){
+$('.hand2').on('click',function (){
 
-          $(".audio1").toggleClass('show')
+          $(".yellow2").toggleClass('slide1')
+
+})
+$('.shape1_1').on('click',function (){
+
+          $(".link3").toggleClass('show')
 
 })
 $('.shape_6').on('click',function (){
@@ -171,11 +181,29 @@ $('.close').on('click',function (){
           $('.link8').removeClass('show')
             $('.link1').removeClass('show')
             $('.link6').removeClass('show')
-
+              $('.link3').removeClass('show')
+              $('.link7').removeClass('show')
+                $('.link6_2').removeClass('show')
+$('.link1_1').removeClass('show')
 })
 $('.shape_2').on('click',function (){
 // $('.link8').css('background-color', 'red')
           $('.link1').addClass('show')
+
+})
+$('.shape_2_1').on('click',function (){
+// $('.link8').css('background-color', 'red')
+          $('.link1_1').addClass('show')
+
+})
+$('.shape_7').on('click',function (){
+// $('.link8').css('background-color', 'red')
+          $('.link7').addClass('show')
+
+})
+$('.shape_6_2').on('click',function (){
+// $('.link8').css('background-color', 'red')
+          $('.link6_2').addClass('show')
 
 })
 //   $(window).resize(function(){
